fbadaf5b3ee6d09af06557db69387ffc5d34c718
Rafael Farias <rafaelfarias@zohomail.com>
2022-09-10 22:28:19 -0300

    feat(styles): add rubik font and changed styles to it

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,24 +1,24 @@
-import { FontAwesome } from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import * as RubikFont from "@expo-google-fonts/rubik";
+import { FontAwesome } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
 
-  // Load any resources or data that we need prior to rendering the app
   useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
         SplashScreen.preventAutoHideAsync();
 
-        // Load fonts
         await Font.loadAsync({
           ...FontAwesome.font,
-          'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          rubik: RubikFont.Rubik_400Regular,
+          "rubik-semibold": RubikFont.Rubik_600SemiBold,
+          "rubik-bold": RubikFont.Rubik_700Bold,
         });
       } catch (e) {
-        // We might want to provide this error information to an error reporting service
         console.warn(e);
       } finally {
         setLoadingComplete(true);
